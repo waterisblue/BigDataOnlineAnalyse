ea5d8e2a4c1fcdb57c6786b13cd55115a7f54913
waterIB <1772756410@qq.com>
2024-01-26 15:17:51 +0800

    随便写了点前端，上班没空写奥！！！

--- Fronted/dataanalysisfronted/src/views/InfoView.vue
+++ Fronted/dataanalysisfronted/src/views/InfoView.vue
@@ -1,13 +1,34 @@
 <template>
-    <div class="column">
-        <div class="row justify-left welcomeText">inFo</div>
+    <div class="q-pa-md">
+        <div class="column q-gutter-md" style="height: 80vh;">
+            <div class="col-3 avatar" style="background-color: aliceblue;">
+                <div class="row items-center q-gutter-xs" style="height: 100%;">
+                    <div class="inner_avatar col-1">
+                        <img src="https://cdn.quasar.dev/img/avatar.png">
+                    </div>
+                    <div class="nickname col-2">
+                        waterIB
+                    </div>
+                    <div class="col-auto">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-8 description" style="background-color: black;">
+                    
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    .welcomeText {
-        font-size: 2rem;
-        font-weight: bold;
-        margin: 1rem 0 0 1rem;
+.inner_avatar {
+    margin-left: 1rem;
+    img {
+        border-radius: 2rem;
     }
+    .nickname {
+        
+    }
+}
 </style>
